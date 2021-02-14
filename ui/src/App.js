cef624063f6955284140347518279a60c49c4c7d
Vishal Patel <vishal41997@gmail.com>
2021-02-14 18:31:48 -0500

    Initial setup of front end
    installed reactjs, react bootstrap, material ui

---
+++ ui/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+
+
+function App() {
+  return (
+    <div className="App">
+     Wellness Tracking App
+        <Button> Hello </Button>
+    </div>
+  );
+}
+
+export default App;
