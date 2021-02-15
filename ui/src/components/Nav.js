b8587636822224d29376c0b0cd0f96ebc6853d9e
Vishal Patel <vishal41997@gmail.com>
2021-02-14 23:33:57 -0500

    Login Template

---
+++ ui/src/components/Nav.js
@@ -0,0 +1,25 @@
+import {Link} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+
+
+export default function navigation(){
+
+    return (
+
+        <>
+
+            <div style={{float:'right',paddingTop:'5px',width:'100%',paddingBottom:'10px'}}>
+                <Button variant="outline-primary"> <Link to={"/"} > Home </Link></Button>{' '}
+
+                <Button variant="outline-primary" > <Link to={"/login"}> Login </Link> </Button> {''}
+
+                <Button variant="outline-primary"> <Link to={"/about"}> About </Link> </Button> {''}
+
+            </div>
+
+        </>
+    )
+
+
+}
---
+++ ui/src/components/placeholder.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function placeholder(){
+    return(
+        <>
+            Add Page
+        </>
+    )
+}
