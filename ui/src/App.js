dd9cb43a044ebb93bf75b0c9996376c104f926c4
Vishal Patel <vishal41997@gmail.com>
2021-02-14 23:33:57 -0500

    Login Template

--- ui/src/App.js
+++ ui/src/App.js
@@ -3,13 +3,29 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'react-bootstrap';
 
+import Nav from './components/Nav';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Placeholder from "./components/placeholder";
+
+import Login from "./components/registeration/login";
 
 function App() {
   return (
-    <div className="App">
-     Wellness Tracking App
-        <Button> Hello </Button>
-    </div>
+      <Router>
+
+          <Nav/>
+          <Switch>
+
+              <Route path="/" exact component={Placeholder}/>
+              <Route path="/login" exact component={Login}/>
+              <Route path="/about" exact component={Placeholder}/>
+
+
+
+          </Switch>
+
+
+      </Router>
   );
 }
 
