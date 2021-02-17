7c8885dfc771bf9862f8aee9fe41781a8e240c7b
Vishal Patel <vishal41997@gmail.com>
2021-02-16 23:22:54 -0500

    Login Page theme change

--- ui/src/components/registeration/login.js
+++ ui/src/components/registeration/login.js
@@ -3,105 +3,108 @@ import React,{useState,useEffect} from 'react';
 import {Card,Button} from 'react-bootstrap';
 import {TextField} from '@material-ui/core/';
 
+import validator from 'validator';
+
 export default function Login(props){
 
 
     const [username,setUsername] = useState();
     const [password,setPassword] = useState();
+
+
     const [isLoggedIn,setisLoggedIn] = useState(false);
 
     useEffect(()=>{
         if ( localStorage.getItem('userName') )
-            {
-                setisLoggedIn(true)
-                setUsername(localStorage.getItem('userName'))
-                props.history.push('/about')
-            }
+        {
+            setisLoggedIn(true)
+            setUsername(localStorage.getItem('userName'))
+            props.history.push('/about')
+        }
     })
+
+    const onLogin=()=>{
+
+
+    }
 
 
 
     return (
         <>
 
-            { !isLoggedIn ?
+            <center>
 
-
-                <center>
-
-                    <div class='card' style={{alignItems:'center'}}>
+                <div class='card' style={{alignItems:'center'}}>
 
 
 
-                        <Card bg={'PRIMARY'.toLowerCase()} style={{ width: '18rem' }} >
+                    <Card  bgstyle={{ width: '18rem' , border: 'none'}} >
 
-                            <Card.Body>
+                        <div className={'row'}>
 
-                                <TextField
-                                    required
-                                    style={{background:'white'}}
-                                    color={"secondary"}
-                                    label="Username"
-                                    variant="filled"
-                                    value={username}
-                                    onChange={(e)=>setUsername(e.target.value)}
-                                />
+                            <div className={'col-md-7'}>
+                                <img src={process.env.PUBLIC_URL + '/Meditation.PNG'} />
+                            </div>
 
-                                <br/>
-                                <br/>
+                            <div className={'col-md-5'}>
 
-                                <TextField
-                                    required
-                                    style={{background:'white'}}
-                                    color={"secondary"}
-                                    label="Password"
-                                    variant="filled"
-                                    type={"password"}
-                                    value={password}
-                                    onChange={(e)=>setPassword(e.target.value)}
+                                <h3 style={{fontSize:'20px', backgroundColor: 'white !important'}}> <b> <strong> Login </strong></b>  </h3>
 
-                                />
+                                <Card.Body>
 
-                                <br/>
-                                <br/>
+                                    <TextField
+                                        required
+                                        style={{background:'white !important'}}
+                                        color={"secondary"}
+                                        label="Username"
+                                        // variant="filled"
+                                        value={username}
+                                        onChange={(e)=>setUsername(e.target.value)}
+                                    />
 
-                                <Button type="button" variant="success" onClick={()=>{
-                                    setisLoggedIn(true)
+                                    <br/>
+                                    <br/>
 
-                                    props.history.push('/about')
-                                }}> Login </Button>
+                                    <TextField
+                                        required
+                                        style={{background:'white !important'}}
+                                        color={"secondary"}
+                                        label="Password"
+                                        // variant="filled"
+                                        type={"password"}
+                                        value={password}
+                                        onChange={(e)=>setPassword(e.target.value)}
 
-                                <br/>
+                                    />
 
-                                <p onClick={()=> props.history.push('/signup')}>
-                                    New User Sign Up Here
-                                </p>
+                                    <br/>
+                                    <br/>
 
-                            </Card.Body>
-                        </Card>
+                                    <Button style={{width:'100px'}} type="button" variant="success" onClick={()=>{
+                                        onLogin()
+                                    }}> Signup </Button>
 
+                                    <br/>
+                                    <br/>
 
-
-
-
-                    </div>
-                </center>
-
-               :
-
-                <Card bg={'Success'.toLowerCase()}  text={'white'} style={{display: 'block',
-                    marginLeft: 'auto',
-                    marginRight: 'auto'}}>
-
-                    <Card.Body>
-
-                        Already Logged In
-
-                    </Card.Body>
-                </Card>
+                                    <p onClick={()=>props.history.push('/signup')}> New User Sign Up Here </p>
 
 
-            }
+                                </Card.Body>
+                            </div>
+
+                        </div>
+                    </Card>
+
+
+
+
+
+                </div>
+            </center>
+
+
         </>
     )
 
