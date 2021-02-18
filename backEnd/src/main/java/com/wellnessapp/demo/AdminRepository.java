25da40d3fadf16aa3c1068268928c54c14a42574
Lucas Forbes <lucasforbes13@gmail.com>
2021-02-18 14:19:30 -0500

    Set up post and get for Admin and Creator databases. Ready for front end calls

--- backEnd/src/main/java/com/wellnessapp/demo/AdminRepository.java
+++ backEnd/src/main/java/com/wellnessapp/demo/AdminRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
 
-public interface AdminRepository extends MongoRepository<Admin, Long> {
+public interface AdminRepository extends MongoRepository<Admin, Integer> {
     // possible functions for querrying the db
     public Admin findByUsername(String username);
     public List<Admin> findByPhone(String phone);
