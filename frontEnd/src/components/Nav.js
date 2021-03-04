7c48c1228d96a67c4309da03f21a730498e46ec8
Vishal Patel <vishal41997@gmail.com>
2021-03-04 09:17:50 -0600

    Login and Logout headers

--- frontEnd/src/components/Nav.js
+++ frontEnd/src/components/Nav.js
@@ -1,9 +1,21 @@
 import {Link} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'react-bootstrap';
+import {useEffect, useState} from "react";
 
 
-export default function navigation(props){
+export default function Navigation(props){
+
+
+    const[userLoggedIn,setUserLoggedIn] =  useState(false);
+
+    useEffect(()=>{
+        if ( localStorage.getItem('email') )
+        {
+            setUserLoggedIn(true)
+        }
+    })
+
 
     return (
 
@@ -12,13 +24,18 @@ export default function navigation(props){
             <div style={{float:'right',paddingTop:'5px',width:'100%',paddingBottom:'10px'}}>
                 <Button variant="outline-primary"> <Link to={"/"} > Home </Link></Button>{' '}
 
-                <Button variant="outline-primary" > <Link to={"/login"}> Login </Link> </Button> {''}
-
+                {userLoggedIn?<></>:
+                    <>
+                    <Button variant="outline-primary" > <Link to={"/login"}> Login </Link> </Button> {''}
+                    </>
+                    }
                 <Button variant="outline-primary" > <Link to={"/signup"}> Signup </Link> </Button> {''}
 
 
                 <Button variant="outline-primary"> <Link to={"/about"}> About </Link> </Button> {''}
 
+
+                {userLoggedIn?
                 <Button variant="outline-primary" onClick={()=>{
 
                     alert("Logged out")
@@ -28,7 +45,12 @@ export default function navigation(props){
 
                     }, 2000);
 
-                }}>  Logout </Button> {''}
+                }}>  Logout </Button>
+                    :
+                    <>
+                    </>
+                }
+
 
 
 
