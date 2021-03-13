9a5dee4b16d70740734032e0bb5127f8f1620e67
Wu1072846210 <40910812+Wu1072846210@users.noreply.github.com>
2021-03-13 23:04:00 +0800

    debug

--- backEnd/src/main/java/com/wellnessapp/demo/tools/ImageDownload.java
+++ backEnd/src/main/java/com/wellnessapp/demo/tools/ImageDownload.java
@@ -6,7 +6,9 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class ImageDownload {
 
     @Autowired
