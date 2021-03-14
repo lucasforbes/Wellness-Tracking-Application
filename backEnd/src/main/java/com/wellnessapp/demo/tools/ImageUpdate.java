40ca404eee6cdccdb24d128619c561cbe806db38
Wu1072846210 <40910812+Wu1072846210@users.noreply.github.com>
2021-03-14 18:23:58 +0800

    Image operation
    1. upload image and download image
    2. details of tools, description of interface

--- backEnd/src/main/java/com/wellnessapp/demo/tools/ImageUpdate.java
+++ backEnd/src/main/java/com/wellnessapp/demo/tools/ImageUpdate.java
@@ -1,5 +1,6 @@
 package com.wellnessapp.demo.tools;
 
+import com.google.gson.JsonArray;
 import org.bson.types.Binary;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
