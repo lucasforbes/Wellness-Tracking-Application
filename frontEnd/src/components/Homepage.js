d72de593061d7f32de14932026c2a8d4502efc18
anafe03 <79051185+anafe03@users.noreply.github.com>
2021-03-15 13:04:08 -0400

    made homepage dynamic

--- frontEnd/src/components/Homepage.js
+++ frontEnd/src/components/Homepage.js
@@ -25,137 +25,97 @@ export default function Homepage() {
     return (
         <>
 
-
-
-            <Table>
-
-                <Card className="bg-dark text-white">
+            <Row style = {{}}>
+                <Card style = {{width: '100%', height:'100%'}} className="bg-dark text-white">
                     <Card.Img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fchinesefloweranthology.files.wordpress.com%2F2015%2F04%2Flotus-flowers-wallpaper-hd-21.jpg&f=1&nofb=1" alt="Card iimage" />
-                    <Card.ImgOverlay>;
+                    <Card.ImgOverlay style = {{width: '100%'}}>
 
-                     <div className={'row'}>
-
+                     <div className={'row'} style={{}}>
                          <div className={'col-md-8'}>
-                             <Card.Title style={{ fontSize: '1000%', border: '2000px', textAlign: 'center', color: 'White', position: 'relative', left: '500px'}}>Bloom Wellness</Card.Title>
-                             <Card.Text  style={{ fontSize: '200%',  textAlign: 'center', color: 'white',  position: 'relative', left: '500px'}}>
-                                 Eat well, Train wellll, Be well
+                             <Card.Title style={{ fontSize: '1000%', textAlign: 'center', color: 'White', position: 'relative', left: '43%',bottom: '-30%'}}>Bloom Wellness</Card.Title>
+                             <Card.Text  style={{ fontSize: '200%',  textAlign: 'center', color: 'white',  position: 'relative', left: '43%', bottom: '-30%'}}>
+                                 Eat well, Train well, Be well
                              </Card.Text>
                          </div>
-
-
-
-
                      </div>
 
                      <div className={'row'}>
-                         <div className={'col-md-2'} style={{color:'black', position: 'relative', bottom: '110px', right: '-200px'}}>
+                         <div className={'col-md-2'} style={{color:'black', position: 'relative', bottom: '30%', right: '-11%'}}>
                              <Login/>
                          </div>
                      </div>
-
                     </Card.ImgOverlay>
-
                 </Card>
+            </Row>
 
+            <Row style = {{fontSize: "200%"}}>
+                <Card style={{backgroundColor: 'grey', width: '100%'}}>
+                    /
+                </Card>
+            </Row>
 
-                <tbody>
-                <Container fluid style={{backgroundColor: 'white'}}>
-                    <Row style = {{fontSize: "200%"}}>
-                        <Card style={{backgroundColor: 'lightgrey', width: '100%'}}>
-                            /
-                        </Card>
-                    </Row>
+            <Row style={{backgroundColor: 'lightgrey', borderStyle: 'square', padding: '3%'}}>
+                <Col xs = {4}>
+                    <Card style={{width: '80%', borderColor: 'Red Orange', borderWidth: '7px', height:'100%', position: 'relative', left: '9%'}}>
+                        <Card.Img style={{height: '70%'}} variant="top" src="Exercise_Icon.jpg"/>
+                        <Card.Body>
+                            <Card.Title>Exercise Programs</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">Train Well</Card.Subtitle>
+                            <Card.Text>Find exercise programs here</Card.Text>
+                            <Card.Link href="#">click here</Card.Link>
+                        </Card.Body>
+                    </Card>
+                </Col>
 
+                <Col xs = {4}>
+                    <Card style={{width: '80%', borderWidth: '7px', borderColor: 'Blue Green', position: 'relative', left: '9%', height: '100%'}} bg='light'>
+                        <Card.Img style={{height: '70%'}} variant="top" src="forksandknivesss.jpg"/>
+                        <Card.Body>
+                            <Card.Title>Diet Programs</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">Eat Well</Card.Subtitle>
+                            <Card.Text>Find diet programs here</Card.Text>
+                            <Card.Link href="#">click here</Card.Link>
+                        </Card.Body>
+                    </Card>
+                </Col>
 
-                    <Row style={{backgroundColor: 'lightgrey', borderStyle: 'square', padding: '20px'}}>
-                        <Col>
-                            <Card style={{width: '350px', borderColor: 'Red Orange', borderWidth: '2px'}}>
-                                <Card.Img style={{height: '19rem'}} variant="top" src="Exercise_Icon.jpg"/>
-                                <Card.Body>
-                                    <Card.Title>Exercise Programs</Card.Title>
-                                    <Card.Subtitle className="mb-2 text-muted">Train Well</Card.Subtitle>
-                                    <Card.Text>
-                                        Find exercise programs here
-                                    </Card.Text>
-                                    <Card.Link href="#">click here</Card.Link>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                        <Col>
+                <Col xs = {4}>
+                    <Card style={{width: '80%', borderColor: 'Purple Blue', borderWidth: '7px', height:'100%', position: 'relative', left: '9%'}} bg='light'>
+                        <Card.Img style={{height: '70%'}} variant="top" src="./Wellness_Icon.jpg"/>
+                        <Card.Body>
+                            <Card.Title>Mental Wellness Programs</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">Be Well</Card.Subtitle>
+                            <Card.Text>Access to wellness programs</Card.Text>
+                            <Card.Link href="#">Card Link</Card.Link>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
 
-                            <Card margin='500px!important'
-                                  style={{width: '350px', borderWidth: '2px', borderColor: 'Blue Green'}} bg='light'>
-                                <Card.Img style={{height: '300px'}} variant="top" src="forksandknivesss.jpg"/>
-                                <Card.Body>
-                                    <Card.Title>Diet Programs</Card.Title>
-                                    <Card.Subtitle className="mb-2 text-muted">Eat Well</Card.Subtitle>
-                                    <Card.Text>
-                                        Find diet programs here
-                                    </Card.Text>
-                                    <Card.Link href="#">click here</Card.Link>
-                                </Card.Body>
-                            </Card>
-                        </Col>
+            <Row style = {{fontSize: "200%"}}>
+                <Card style={{backgroundColor: 'grey', width: '100%'}}>
+                    /
+                </Card>
+            </Row>
 
-                        <Col>
-                            <Card style={{width: '350px', borderColor: 'Purple Blue', borderWidth: '2px'}} bg='light'>
-                                <Card.Img style={{height: '300px'}} variant="top" src="./Wellness_Icon.jpg"/>
-                                <Card.Body>
-                                    <Card.Title>Mental Wellness Programs</Card.Title>
-                                    <Card.Subtitle className="mb-2 text-muted">Be Well</Card.Subtitle>
-                                    <Card.Text>
-                                        Access to wellness programs
-                                    </Card.Text>
-                                    <Card.Link href="#">Card Link</Card.Link>
-                                </Card.Body>
-                            </Card>
-                        </Col>
+            <Row>
+                <Form style={{width: '600px', border: 'solid',borderColor: 'lightgrey', padding: '2%', margin: '4%'}}>
+                    <header style={{ fontSize: '200%', color: 'grey'}}>Contact Us</header>
 
+                    <Form.Group  controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email"/>
+                        <Form.Text className="text-muted"> We will respond to this email when you inquiry is processed</Form.Text>
+                    </Form.Group>
 
+                    <Form.Group controlId="exampleForm.ControlTextarea1">
+                        <Form.Label>Message</Form.Label>
+                        <Form.Control as="textarea" rows={3} placeholder={"Enter message here"}/>
+                    </Form.Group>
 
-                    </Row>
-                    <Row>
-                        <Col>
-                            want to add spacing here
-                        </Col>
-                        <Col></Col>
-                    </Row>
-
-                    <Form style={{width: '600px', border: 'solid',borderColor: 'lightgrey', padding: '10px'}}>
-                        <header style={{ fontSize: '200%', color: 'grey'}}>
-                            Contact Us
-                        </header>
-
-                        <Form.Group  controlId="formBasicEmail">
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email"/>
-                            <Form.Text className="text-muted">
-                                We will respond to this email when you inquiry is processed
-                            </Form.Text>
-                        </Form.Group>
-
-                        <Form.Group controlId="exampleForm.ControlTextarea1">
-                            <Form.Label>Message</Form.Label>
-                            <Form.Control as="textarea" rows={3} placeholder={"Enter message here"}/>
-                        </Form.Group>
-
-
-                        <Button variant="primary" type="submit">
-                            Submit
-                        </Button>
-                    </Form>
-
-
-                </Container>
-
-                <Container>
-
-
-                </Container>
-                </tbody>
-
-
-            </Table>
+                    <Button variant="primary" type="submit">Submit</Button>
+                </Form>
+            </Row>
 
 
 
