d1d9a867f5b05597ac03079b7d16f6333fe3ca75
Vishal Patel <vishal41997@gmail.com>
2021-03-26 14:00:30 -0500

    trying exercise to update

--- backEnd/src/main/java/com/wellnessapp/demo/Exersize/ExersizeController.java
+++ backEnd/src/main/java/com/wellnessapp/demo/Exersize/ExersizeController.java
@@ -32,19 +32,19 @@ public class ExersizeController {
     @PostMapping("/addExersize")
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @ResponseBody
-    public Exersize saveExersize(@RequestParam("photo") MultipartFile photo,@RequestPart("exersize") String exersize1) throws JsonProcessingException {
+    public Exersize saveExersize(@RequestParam("photo") MultipartFile photo,@RequestPart("exersize") String exersize) throws JsonProcessingException {
         System.out.println("");
         int id = edb.findAll().size();
-        Exersize exersize = new ObjectMapper().readValue(exersize1, Exersize.class);
-        exersize.id = (id + 1);
+        Exersize exersize1 = new ObjectMapper().readValue(exersize, Exersize.class);
+        exersize1.id = (id + 1);
         try{
             MultipartFile file = photo;
             int count2 = idb.findAll().size();
             Image image = new Image();
             image.setId(count2);
             image.setName(file.getOriginalFilename());
-            image.setUserEmail(exersize.getEmail());
-            image.setOtherDbId(exersize.getId());
+            image.setUserEmail(exersize1.getEmail());
+            image.setOtherDbId(exersize1.getId());
             image.setBelongsTo(2);
             image.setUpdateDate(new Date());
             image.setContent(new Binary(file.getBytes()));
@@ -52,12 +52,12 @@ public class ExersizeController {
             image.setSize(file.getSize());
             Image savedFile = idb.save(image);
             String url = "https://bloom-wellness-back.herokuapp.com/file/image/" + savedFile.getId();
-            exersize.setPicture(url);
+            exersize1.setPicture(url);
         }catch (IOException e){
             e.printStackTrace();
         }
-        edb.save(exersize);
-        return exersize;
+        edb.save(exersize1);
+        return exersize1;
     }
 
 //    @GetMapping("/findExersizeByCreatorEmail/{email, name}")
