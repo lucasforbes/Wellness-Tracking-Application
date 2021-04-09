475e31cd824b9a2c00b3c4a8e2f7755f318a7a4c
Vishal Patel <vishal41997@gmail.com>
2021-04-08 23:11:58 -0500

    Approve and reject exercises by Admin

--- frontEnd/src/components/admin/AdminDashboard.js
+++ frontEnd/src/components/admin/AdminDashboard.js
@@ -1,9 +1,14 @@
 import  React,{useState,useEffect} from 'react';
 import axios from "axios";
 import {act} from "@testing-library/react";
+import {Button} from "react-bootstrap";
+import {FcCancel,FcCheckmark} from  "react-icons/fc";
+import {Select, MenuItem, InputLabel} from "@material-ui/core";
 
 export default function AdminDashboard(props){
 
+
+    const [rerender,setRerender] = useState(false);
 
     const [creatorsContent,setCreatorsContent] = useState();
 
@@ -19,10 +24,38 @@ export default function AdminDashboard(props){
             })
 
 
-    },[])
+    },[rerender])
 
 
+    // const [filterEmail,setFilterEmail] = useState();
 
+    // let newFilter = null;
+    // if(creatorsContent){
+    //     newFilter   = JSON.parse(JSON.stringify(creatorsContent));
+    //     // if(filterEmail && filterEmail!="All"){
+    //     //    newFilter = null
+    //     //     newFilter={'email':creatorsContent['filterEmail']}
+    //     // }
+    // }
+
+
+    const updateApproval=(id,status)=>{
+
+        let json = JSON.stringify({
+          'id': id,
+          'status': status
+        })
+
+        axios.post('https://bloom-flask-app.herokuapp.com/updateApprovalExercise',json)
+            .then((res)=>{
+                setRerender(!rerender)
+                console.log(res.data)
+            })
+            .catch((err)=>{
+                alert("Error while changing approval status")
+                console.log(err)
+            })
+    }
 
 
     return(
@@ -30,6 +63,17 @@ export default function AdminDashboard(props){
         <>
 
             <h5 className={"card-header text-white bg-info"}> All Workouts </h5>
+
+            {/*<InputLabel> Filter Creator </InputLabel>*/}
+            {/*<select value={} onChange={(e)=>setFilterEmail(e.target.value)}>*/}
+            {/*    <option value={"All"}> All </option>*/}
+            {/*    {creatorsContent ?  Object.keys(creatorsContent).map((key,index) => {  return(*/}
+            {/*           <option value={key}> {key} </option>*/}
+            {/*       )*/}
+            {/*}):""*/}
+            {/*}*/}
+            {/*</select>*/}
+
 
             {creatorsContent ?
                 <>
@@ -39,21 +83,42 @@ export default function AdminDashboard(props){
                             <div className={"card"}>
                             <h5 className={"card-title"}>Creator:  {key} </h5>
 
+
+
                                 <div className={"card-body row"}>
 
                                 { creatorsContent[key].map((exercise,index2)=>{
                                 return(
                                     <>
+
                                     <div id={index2}  className={exercise.approved?"bg-success text-white col-md-5 card mb-3":"bg-danger text-white col-md-5 card mb-3"} style={{maxWidth: "50rem", minHeight:"10rem"}}>
-                                        {exercise.title}  {" "+exercise.paid}
+                                        <div className={"row"}>
+                                            <div className={"col-md-6"}> <br/> <label>Title: </label> {exercise.title}
+                                            <p>  {exercise.description} </p>
+                                            </div>
+                                            <div className={"col-md-6"}>
+                                            <br/>
+                                                {exercise.approved? <Button style={{width:"max-content"}} variant={"light"} onClick={()=>updateApproval(exercise._id,false)}><FcCancel/> Cancel Approval</Button>: <Button variant={"light"} style={{width:"max-content"}} onClick={()=>updateApproval(exercise._id,true)}><FcCheckmark/> Approve </Button>}
+                                               <p>  <br/><text className={"bg-warning"}>{" Note: "} </text> </p>
+                                                <tr>
+                                                    <td> <input type={'text'} value={exercise.notes?exercise.notes:""} />  </td>
+                                                    <td> {" "}</td>
+                                                    <td>  <Button style={{width:"max-content"}} variant={"info"}> Update </Button> </td>
+                                                </tr>
+
+                                            </div>
+                                        </div>
+
                                         <br/>
-                                       <p>  {exercise.description} </p>
+
+
+
                                         <br/>
                                         <div className={"row"}>
                                         {exercise.activityList.map((activity,index3)=>{
                                             return(
                                                 <>
-                                                <div className={"col-md-5 bg-dark"}>
+                                                <div className={ exercise.activityList.length == 1 ? "col-md-8 bg-dark" : "col-md-5 bg-dark "}>
                                                 <b> {activity.activityName}</b> <br/>
                                                     <p> {activity.activityDescription?activity.activityDescription:""}</p> <br/>
                                                     <p> Targeted Body Parts: {activity.bodyPartsTargeted+" "}
