577ac532edf0656e3e2ad29a1cb6f0a3c582e5fe
Vishal Patel <vishal41997@gmail.com>
2021-04-10 22:26:09 -0500

    admin approval of diet plans

--- frontEnd/src/components/admin/AdminDashboard.js
+++ frontEnd/src/components/admin/AdminDashboard.js
@@ -79,12 +79,51 @@ export default function AdminDashboard(props){
     }
 
 
+    const updateApprovalDiet=(id,status)=>{
+
+        let json = JSON.stringify({
+            'id': id,
+            'status': status
+        })
+
+        axios.post('https://bloom-flask-app.herokuapp.com/updateApprovalDiet',json)
+            .then((res)=>{
+                setRerender(!rerender)
+                console.log(res.data)
+            })
+            .catch((err)=>{
+                alert("Error while changing approval status")
+                console.log(err)
+            })
+    }
+
+
+    const updateApprovalNotesDiet=(id)=>{
+
+        let notes = document.getElementById(id).value;
+
+        let json = JSON.stringify({
+            'id': id,
+            'notes': notes
+        })
+
+        axios.post('https://bloom-flask-app.herokuapp.com/updateApprovalDietNote',json)
+            .then((res)=>{
+                setRerender(!rerender)
+                console.log(res.data)
+            })
+            .catch((err)=>{
+                alert("Error while changing approval notes")
+                console.log(err)
+            })
+    }
+
 
     return(
 
         <>
 
-            <h5 className={"card-header text-white bg-info"}> All Workouts </h5>
+            <h5 className={"card-header text-white bg-info"}> All Workouts and Diets </h5>
 
             {/*<InputLabel> Filter Creator </InputLabel>*/}
             {/*<select value={} onChange={(e)=>setFilterEmail(e.target.value)}>*/}
@@ -113,52 +152,111 @@ export default function AdminDashboard(props){
                                 return(
                                     <>
 
-                                    <div id={index2}  className={exercise.approved?"bg-success text-white col-md-5 card mb-3":"bg-danger text-white col-md-5 card mb-3"} style={{maxWidth: "50rem", minHeight:"10rem"}}>
-                                        <div className={"row"}>
-                                            <div className={"col-md-6"}> <br/> <label>Title: </label> {exercise.title}
-                                            <p>  {exercise.description} </p>
+                                        {exercise.activityList ? <>
+                                            <div id={index2}  className={exercise.approved?"bg-success text-white col-md-5 card mb-3":"bg-danger text-white col-md-5 card mb-3"} style={{maxWidth: "50rem", minHeight:"10rem"}}>
+                                                <div className={"row"}>
+                                                    <div className={"col-md-6"}> <br/> <label>Title: </label> {exercise.title}
+                                                    <p>  {exercise.description} </p>
+                                                    </div>
+                                                    <div className={"col-md-6"}>
+                                                    <br/>
+                                                        {exercise.approved? <Button style={{width:"max-content"}} variant={"light"} onClick={()=>updateApproval(exercise._id,false)}><FcCancel/> Cancel Approval</Button>: <Button variant={"light"} style={{width:"max-content"}} onClick={()=>updateApproval(exercise._id,true)}><FcCheckmark/> Approve </Button>}
+                                                       <p>  <br/><text className={"bg-warning"}>{" Note: "} </text> </p>
+                                                        <tr>
+                                                            <td> <input type={'text'} id={exercise._id} defaultValue={exercise.notes?exercise.notes:""} />  </td>
+                                                            <td> {" "}</td>
+                                                            <td>  <Button style={{width:"max-content"}} variant={"info"} onClick={()=>updateApprovalNotes(exercise._id)}> Update </Button> </td>
+                                                        </tr>
+
+                                                    </div>
+                                                </div>
+
+                                                <br/>
+
+
+
+                                                <br/>
+                                                <div className={"row"}>
+                                                {exercise.activityList.map((activity,index3)=>{
+                                                    return(
+                                                        <>
+                                                        <div className={ exercise.activityList.length == 1 ? "col-md-8 bg-dark" : "col-md-5 bg-dark "}>
+                                                        <b> {activity.activityName}</b> <br/>
+                                                            <p> {activity.activityDescription?activity.activityDescription:""}</p> <br/>
+                                                            <p> Targeted Body Parts: {activity.bodyPartsTargeted+" "}
+                                                                {"Duration: "+activity.totalDuration+" Sets: "+
+                                                            activity.activitySets+ " Reps: "+ activity.activityReps+" Equipment  "+
+                                                            activity.equipmentNeeded+" Video "+
+                                                            activity.videoLink+" "}
+                                                            </p>
+                                                        </div>
+                                                        <div className={"col-md-1"}>
+                                                        </div>
+                                                        </>
+                                                    )
+                                                })}
+                                                </div>
                                             </div>
-                                            <div className={"col-md-6"}>
-                                            <br/>
-                                                {exercise.approved? <Button style={{width:"max-content"}} variant={"light"} onClick={()=>updateApproval(exercise._id,false)}><FcCancel/> Cancel Approval</Button>: <Button variant={"light"} style={{width:"max-content"}} onClick={()=>updateApproval(exercise._id,true)}><FcCheckmark/> Approve </Button>}
-                                               <p>  <br/><text className={"bg-warning"}>{" Note: "} </text> </p>
-                                                <tr>
-                                                    <td> <input type={'text'} id={exercise._id} defaultValue={exercise.notes?exercise.notes:""} />  </td>
-                                                    <td> {" "}</td>
-                                                    <td>  <Button style={{width:"max-content"}} variant={"info"} onClick={()=>updateApprovalNotes(exercise._id)}> Update </Button> </td>
-                                                </tr>
-
+                                            <div className={"col-md-1"}>
                                             </div>
-                                        </div>
+                                                </>:
 
-                                        <br/>
+                                        ""}
+
+                                        {exercise.dietList ?
+                                            <>
+                                                <div id={index2}  className={exercise.approved?"bg-success text-white col-md-5 card mb-3":"bg-danger text-white col-md-5 card mb-3"} style={{maxWidth: "50rem", minHeight:"10rem"}}>
+                                                    <div className={"row"}>
+                                                        <div className={"col-md-6"}> <br/> <label>Title: </label> {exercise.title}
+                                                            <p>  {exercise.description} </p>
+                                                        </div>
+                                                        <div className={"col-md-6"}>
+                                                            <br/>
+                                                            {exercise.approved? <Button style={{width:"max-content"}} variant={"light"} onClick={()=>updateApprovalDiet(exercise._id,false)}><FcCancel/> Cancel Approval</Button>: <Button variant={"light"} style={{width:"max-content"}} onClick={()=>updateApprovalDiet(exercise._id,true)}><FcCheckmark/> Approve </Button>}
+                                                            <p>  <br/><text className={"bg-warning"}>{" Note: "} </text> </p>
+                                                            <tr>
+                                                                <td> <input type={'text'} id={exercise._id} defaultValue={exercise.notes?exercise.notes:""} />  </td>
+                                                                <td> {" "}</td>
+                                                                <td>  <Button style={{width:"max-content"}} variant={"info"} onClick={()=>updateApprovalNotesDiet(exercise._id)}> Update </Button> </td>
+                                                            </tr>
+
+                                                        </div>
+                                                    </div>
+
+                                                    <br/>
 
 
 
-                                        <br/>
-                                        <div className={"row"}>
-                                        {exercise.activityList.map((activity,index3)=>{
-                                            return(
-                                                <>
-                                                <div className={ exercise.activityList.length == 1 ? "col-md-8 bg-dark" : "col-md-5 bg-dark "}>
-                                                <b> {activity.activityName}</b> <br/>
-                                                    <p> {activity.activityDescription?activity.activityDescription:""}</p> <br/>
-                                                    <p> Targeted Body Parts: {activity.bodyPartsTargeted+" "}
-                                                        {"Duration: "+activity.totalDuration+" Sets: "+
-                                                    activity.activitySets+ " Reps: "+ activity.activityReps+" Equipment  "+
-                                                    activity.equipmentNeeded+" Video "+
-                                                    activity.videoLink+" "}
-                                                    </p>
+                                                    <br/>
+                                                    <div className={"row"}>
+                                                        {exercise.dietList.map((dietItem,index3)=>{
+                                                            return(
+                                                                <>
+
+                                                                        <div className={ exercise.dietList.length == 1 ? "col-md-8 bg-dark" : "col-md-5 bg-dark "}>
+                                                                            <b> {dietItem.item}</b> <br/>
+                                                                            <p> Serving Size: {dietItem.servingSize+" "}
+                                                                                {"Fat(kg): "+dietItem.fat+" Carbs: "+
+                                                                                dietItem.carbs+ " Calories: "+ dietItem.calories+" Protein  "+
+                                                                                dietItem.protein+" "}
+                                                                            </p>
+                                                                        </div>
+                                                                        <div className={"col-md-1"}>
+                                                                        </div>
+
+                                                                        <div className={"col-md-1"}>
+                                                                        </div>
+                                                                </>
+                                                            )
+                                                        })}
+                                                    </div>
                                                 </div>
                                                 <div className={"col-md-1"}>
                                                 </div>
-                                                </>
-                                            )
-                                        })}
-                                        </div>
-                                    </div>
-                                    <div className={"col-md-1"}>
-                                    </div>
+                                            </>:
+
+                                            ""}
+
                                     </>
                                 )
                             })
